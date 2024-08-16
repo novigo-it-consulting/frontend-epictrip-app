c7126b2e8ca642c78077a9ee3cea3f848dea712b
gabrielIungo <gabriel.soares@iungo-iot.com.br>
2024-08-16 17:13:15 -0300

    Implementado a tela de Experience em Places

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -11,7 +11,8 @@ const LARGE_IMAGE_WIDTH = 36;
 const MEDIUM_IMAGE_WIDTH = LARGE_IMAGE_WIDTH * 1;
 const SMALL_IMAGE_WIDTH = MEDIUM_IMAGE_WIDTH * 1;
 
-const ListItem = ({ uri, scrollX, index, dataLength, title, id, onPress }) => {
+const ListItem = ({ uri, scrollX, index, dataLength, title, id, onPress, withIcon }) => {
+
   const inputRange = [
     (index - 2) * SMALL_IMAGE_WIDTH,
     (index - 1) * SMALL_IMAGE_WIDTH,
@@ -48,20 +49,27 @@ const ListItem = ({ uri, scrollX, index, dataLength, title, id, onPress }) => {
     alignSelf: "center", // Alinhar a imagem no meio do card
   }));
 
-  // console.log(sampleData.title)
-
   return (
     <TouchableOpacity onPress={() => onPress(id)}>
-    <Card style={styles.card}>
-      <View style={styles.shadowContainer}>
-        <Animated.Image
+    <Card style={withIcon ? styles.card : stylesWithOutIcon.card}>
+      <View style={withIcon ? styles.shadowContainer : stylesWithOutIcon.shadowContainer}>
+        {withIcon  ? (
+          <>
+          <Animated.Image
           source={{ uri }}
           style={[styles.image, animatedStyle]}
-        />
-        <Card.Title
+          />
+          <Card.Title
           titleStyle={{ fontSize: 12, color: "#172B4D", textAlign: "center", fontWeight: "bold" }}
           title={title}
-        />
+          />
+          </>
+        ) : (
+          <Card.Title
+          titleStyle={{ fontSize: 12, color: "#172B4D", textAlign: "center", fontWeight: "bold" }}
+          title={title}
+          />
+        )}
       </View>
     </Card>
     </TouchableOpacity>
@@ -86,6 +94,36 @@ const styles = StyleSheet.create({
       height: 14,
     },
     height: 110, 
+    width: 95,  
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 10,
+    marginRight: 12,
+    marginLeft: 5,
+    alignItems: 'center',  
+    justifyContent: 'center',  
+  },
+  shadowContainer: {
+    flex: 1,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    textAlign: "center",
+  },
+});
+
+const stylesWithOutIcon = StyleSheet.create({
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 50,
+    shadowColor: "#000",
+    paddingTop: 16,
+    shadowOffset: {
+      width: 0,
+      height: 14,
+    },
+    height: 50, 
     width: 95,  
     shadowOpacity: 0.1,
     shadowRadius: 8,
