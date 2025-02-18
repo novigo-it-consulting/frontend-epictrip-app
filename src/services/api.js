e93bd99d60862088bc96667efa774b1d9fde174b
frns1 <fernandesjoao2009@hotmail.com>
2025-02-18 18:15:52 -0300

    up

--- src/services/api.js
+++ src/services/api.js
@@ -80,7 +80,7 @@ export const requestGetUser = async (userId) => {
     const response = await axios.get(`${BASE_URL}/users/${userId}`, {
       headers,
     });
-    return response;
+    return response.data.data;
   } catch (error) {
     throw error;
   }
@@ -263,7 +263,7 @@ export const requestGetBookingByUser = async (userId) => {
       Authorization: `Bearer ${await AsyncStorage.getItem("token")}`,
     };
     const response = await axios.get(`${BASE_URL}/bookings/user/${userId}`, { headers })
-    return response
+    return response.data.data
   } catch (error) {
     throw error;
   }
