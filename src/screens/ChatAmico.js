5edec44545c30708a58be2a989fb803348a8c06b
frns1 <fernandesjoao2009@hotmail.com>
2025-04-22 14:30:14 -0300

    up

--- src/screens/ChatAmico.js
+++ src/screens/ChatAmico.js
@@ -107,13 +107,21 @@ const ChatScreen = () => {
     }
   };
 
+  function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (char) {
+      const rand = Math.random() * 16 | 0;
+      const value = char === 'x' ? rand : (rand & 0x3 | 0x8);
+      return value.toString(16);
+    });
+  }
+
   useEffect(() => {
     const initializeAmiko = async () => {
       const context = await buildContext();
       const preMessage = await AsyncStorage.getItem("preMessage");
       setClientMessage(preMessage ? String(preMessage) : '');
 
-      const amiko = new Amiko("10000232", context);
+      const amiko = new Amiko(generateUUID(), context);
 
       amiko.onChatStateReceived = (newChatState) => {
         setChatState(newChatState);
