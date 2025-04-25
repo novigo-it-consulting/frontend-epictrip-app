b31eec8ef7bb6f097acdc8cd53b832ef6495b86e
frns1 <fernandesjoao2009@hotmail.com>
2025-04-25 15:22:32 -0300

    üp

--- src/services/amiko/amiko.js
+++ src/services/amiko/amiko.js
@@ -5,7 +5,7 @@ class Amiko {
         this.chatId = '478ae18a-596c-4ee8-b6a7-6422cf2571ca';
         this.token = '99jWB13psRBWs5CwcbssGG';
 
-        this.socket = io(`ws://web-socket.fertech.dev.br`, {
+        this.socket = io(`wss://web-socket.fertech.dev.br`, {
             auth: {
                 instance: {
                     chatId: this.chatId,
